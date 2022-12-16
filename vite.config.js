e881288974e118182a0dd4b3c709129a3838130c
Mathilde <mathilde@krogen5.dk>
2022-12-16 18:25:07 +0100

    sendt

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        program: resolve(__dirname, "src/program.html")
         // about: resolve(__dirname, "src/about.html"),
       },
     },
