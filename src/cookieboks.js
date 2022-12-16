0eddbd3e0445e4cdb2b4dddbf86fb2b762f36ba8
Mathilde <mathilde@krogen5.dk>
2022-12-16 13:48:13 +0100

    cookieboks virker

---
+++ src/cookieboks.js
@@ -0,0 +1,15 @@
+const acceptCookie = document.querySelector("#tilladudvalgte");
+const denyCookie = document.querySelector("#tilladalle");
+const cookieboks = document.querySelector("#cookieboks");
+
+acceptCookie.addEventListener("click", fjernCookieBoks);
+denyCookie.addEventListener("click", fjernCookieBoks);
+
+function fjernCookieBoks() {
+    cookieboks.classList.add("hidden");
+    sessionStorage.setItem("fjerncookieboks", true);
+}
+
+if (sessionStorage.getItem("fjerncookieboks")) {
+    cookieboks.classList.add("hidden");
+}
